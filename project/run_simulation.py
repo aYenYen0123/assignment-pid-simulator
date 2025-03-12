73dfcbd751f85e3e35c5e62f377a721feeb8235e
Yen Lin <a870123tw@gmail.com>
2025-03-12 22:53:32 +0800

    feat: add v_init argument to run_uncontrolled_sim

--- project/run_simulation.py
+++ project/run_simulation.py
@@ -5,11 +5,11 @@ from vehicle_dynamics import Vehicle
 from pid_controller import PIDController
 from simulation import Simulation
 
-def run_uncontrolled_simulation():
+def run_uncontrolled_simulation(v_init=10, duration_s=100):
     """
     Run and plot a simulation without a controller.
     """
-    vehicle = Vehicle(mass_kg=1.0, initial_velocity_mps=10.0, drag_coefficient_kgpm=0.05)
+    vehicle = Vehicle(mass_kg=1.0, initial_velocity_mps=v_init, drag_coefficient_kgpm=0.05)
     sim = Simulation(vehicle=vehicle)
     sim.run(duration_s=100)
     sim.plot_results(filename="uncontrolled_velocity.png")
@@ -49,7 +49,7 @@ def run_pid_controlled_simulation(v_init=10, kp=2.0, ki=0.1, kd=1.0, setpoint=5.
 if __name__ == "__main__":
     # Run the uncontrolled simulation
     print("Running uncontrolled simulation...")
-    # sim_uncontrolled = run_uncontrolled_simulation()
+    sim_uncontrolled = run_uncontrolled_simulation(v_init=6, duration_s=100)
     
     # Run the PID controlled simulation
     print("\nRunning PID controlled simulation...")
